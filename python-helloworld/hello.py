be56b36c9ed8d82fa7728dee63f0b70fcd8b1006
AKlimm <127842308+AKlimm@users.noreply.github.com>
2023-08-20 10:45:50 +0200

    Update hello.py

--- python-helloworld/hello.py
+++ python-helloworld/hello.py
@@ -3,5 +3,5 @@ import time
 while True:
     localtime = time.localtime()
     result = time.strftime("%I:%M:%S %p", localtime)
-    print(result)
-    time.sleep(1)
+    print(result, "penis")
+    time.sleep(5)
